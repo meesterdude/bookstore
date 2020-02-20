e1c4eb4cf17fff1685cac11f6f648f29d9a2a1ff
Russell Jennings <violentpurr@gmail.com>
2020-02-20 07:16:29 -0500

    adds test coverage for api endpoints

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -1,10 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe "Books", type: :request do
-  describe "GET /books" do
-    it "works! (now write some real specs)" do
-      get books_path
-      expect(response).to have_http_status(200)
+  describe "POST /shops/:shop_id/books/:id/sell" do
+    let(:publisher_1) {Publisher.create(name: 'westinghouse')}
+    let(:publisher_)  {Publisher.create(name: 'penguin')}
+    let(:shop_1)      {Shop.create(name: 'amazon')}
+    let(:shop_2)      {Shop.create(name: 'borders')}
+    let(:book_1)      {publisher_1.books.create(title: "1984")}
+    let(:book_2)      {publisher_1.books.create(title: "Dictionary")}
+
+    before {shop_1.shop_books.create(book: book_1, copies_in_stock: 5)}
+    context "success" do
+      it "returns successful response" do
+        post shop_book_sell_path(shop_1, book_1, params: {count: 1})
+        expect(response).to have_http_status(202)
+      end
+      it "decrements copies_in_stock for shop_book" do
+        post shop_book_sell_path(shop_1, book_1, params: {count: 1})
+        expect(shop_1.shop_books.first.copies_in_stock).to eql 4
+      end
+      it "increments books_sold_count" do
+        post shop_book_sell_path(shop_1, book_1, params: {count: 1})
+        expect(shop_1.reload.books_sold_count).to eql 1
+      end
+   end
+
+   context "failure" do
+    it "fails if count exceeds available stock" do
+      post shop_book_sell_path(shop_1, book_1, params: {count: 6})
+      expect(response.successful?).to eql false
+      expect(shop_1.reload.books_sold_count).to eql 0
     end
+
+    it "fails if book not in shop" do
+      expect {post shop_book_sell_path(shop_1, book_2, params: {count: 1}) }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+    
+   end
   end
 end
--- spec/requests/publishers_spec.rb
+++ spec/requests/publishers_spec.rb
@@ -1,10 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe "Publishers", type: :request do
-  describe "GET /publishers" do
-    it "works! (now write some real specs)" do
-      get publishers_path
-      expect(response).to have_http_status(200)
+  describe "GET /publishers/:id" do
+    let(:publisher_1) {Publisher.create(name: 'westinghouse')}
+    let(:publisher_)  {Publisher.create(name: 'penguin')}
+    let(:shop_1)      {Shop.create(name: 'amazon')}
+    let(:shop_2)      {Shop.create(name: 'borders')}
+    let(:book_1)      {publisher_1.books.create(title: "1984")}
+
+    it "returns successful response" do
+      get publisher_path(publisher_1.id, format: :json)
+      json = JSON.parse(response.body)
+      expect(response.successful?).to be true
+    end
+
+    it "orders shops by books sold" do
+      shop_1.shop_books.create(book: book_1, copies_in_stock: 5)
+      shop_2.shop_books.create(book: book_1, copies_in_stock: 5)
+      shop_2.update(books_sold_count: 5)
+
+      get publisher_path(publisher_1.id, format: :json)
+      json = JSON.parse(response.body)
+      expect(json["shops"].first["id"]).to eql shop_2.id
+    end
+
+    it "does not show shops not selling a publishers book" do
+      shop_1.shop_books.create(book: book_1, copies_in_stock: 5)
+      shop_2.shop_books.create(book: book_1, copies_in_stock: 5)
+      bad_shop = Shop.create(name: 'ye olde books')
+
+      get publisher_path(publisher_1.id, format: :json)
+      json = JSON.parse(response.body)
+      expect(json["shops"].collect{|shop| shop['id']}).to contain_exactly(shop_1.id, shop_2.id)
     end
   end
 end
