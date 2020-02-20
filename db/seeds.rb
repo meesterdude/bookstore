0b2dd373f43ac1074b0690a2f8e773b28e8477c7
Russell Jennings <violentpurr@gmail.com>
2020-02-20 04:01:02 -0500

    populates seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+publisher = Publisher.create(name: 'westinghouse')
+publisher.books.create(title: '1984')
+shop = Shop.create(name: 'amazon')
+publisher.books.last.shop_books.create(shop: shop, copies_in_stock: 3)
